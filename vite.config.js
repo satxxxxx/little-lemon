c16a16532638b6836d66c671b429b46aa27efdb9
satxxxxx <satxxxxx@yahoo.it>
2025-03-19 21:04:20 +0100

    i have changed the name of the local folder because it was too long

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,  // Cambia da 3000 a 4000
-    https: false,
-    strictPort: false, // Permetti a Vite di cercare un'altra porta se 4000 è occupata
-  }
+  base: '/little-lemon/'  // Aggiungi questa riga
 })
